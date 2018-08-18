12275801bb122b7be14d4994131ef280976138c9
Iulia Eliza Sabadas <iuliaelizamts@gmail.com>
2018-08-18 08:42:44 +0200

    minor change

--- src/App.js
+++ src/App.js
@@ -16,89 +16,89 @@ class App extends Component {
       selectedMarker: {},//the selected marker object
       iconURL: "https://png.icons8.com/ultraviolet/40/000000/marker.png",// the icon for the marker on the map
       markerDescription: '',// details for the selected marker
-      sideBarStyle: {display: 'block'}//style for the sidebar
+      sideBarStyle: {display: 'inherit'}//style for the sidebar
     }
 
-    //fetches the places to be marked on the map using foursquareAPI
-    fetchPlaces = () => {
-        this.fetchMuseums();
-        this.fetchCinemas();
-        this.fetchParks();
-        this.fetchTheaters();
-      }
+  //fetches the places to be marked on the map using foursquareAPI
+  fetchPlaces = () => {
+      this.fetchMuseums();
+      this.fetchCinemas();
+      this.fetchParks();
+      this.fetchTheaters();
+    }
 
-    //fetches the museums in the area
-    fetchMuseums = () => {
-        fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=1500&query=muzeu&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
-        .then(result => result.json())
-        .then(fetchedMuseums => {
-            //if there are items in the venue, adds them to the museums state
-            if(fetchedMuseums.response.venues.length >0){
-              this.setState({museums: fetchedMuseums.response.venues});
-              //also sets a new proprety 'markerType' for each venue
-              this.state.museums.forEach(item => {
-                  item.markerType = 'museum';
-              });
-              return this.state.museums;
-            }else{
-              alert("Sorry we couldn't load the data for Museums. Please try again");
-            }
-          }).catch(err => console.log (err));
-      }
+  //fetches the museums in the area
+  fetchMuseums = () => {
+      fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=1500&query=muzeu&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
+      .then(result => result.json())
+      .then(fetchedMuseums => {
+          //if there are items in the venue, adds them to the museums state
+          if(fetchedMuseums.response.venues.length >0){
+            this.setState({museums: fetchedMuseums.response.venues});
+            //also sets a new proprety 'markerType' for each venue
+            this.state.museums.forEach(item => {
+                item.markerType = 'museum';
+            });
+            return this.state.museums;
+          }else{
+            alert("Sorry we couldn't load the data for Museums. Please try again");
+          }
+        }).catch(err => console.log (err));
+    }
 
-    //fetches the cinemas in the area
-    fetchCinemas = () => {
-        fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=2000&query=cinema&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
-        .then(result => result.json())
-        .then(fetchedCinemas => {
-            //if there are items in the venue, adds them to the cinemas state
-            if(fetchedCinemas.response.venues.length >0){
-              this.setState({cinemas: fetchedCinemas.response.venues});
-              this.state.cinemas.forEach(item => {
-                  item.markerType = 'cinema';
-              });
-              return this.state.cinemas;
-            }else{
-              alert("Sorry we couldn't load the data for Cinemas. Please try again");
-            }
-          }).catch(err => console.log (err));
-      }
+  //fetches the cinemas in the area
+  fetchCinemas = () => {
+      fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=2000&query=cinema&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
+      .then(result => result.json())
+      .then(fetchedCinemas => {
+          //if there are items in the venue, adds them to the cinemas state
+          if(fetchedCinemas.response.venues.length >0){
+            this.setState({cinemas: fetchedCinemas.response.venues});
+            this.state.cinemas.forEach(item => {
+                item.markerType = 'cinema';
+            });
+            return this.state.cinemas;
+          }else{
+            alert("Sorry we couldn't load the data for Cinemas. Please try again");
+          }
+        }).catch(err => console.log (err));
+    }
 
-    //fetches the parks in the area
-    fetchParks = () => {
-        fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=2000&query=parcul&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
-        .then(result => result.json())
-        .then(fetchedParks => {
-            //if there are items in the venue, adds them to the parks state
-            if(fetchedParks.response.venues.length >0){
-              this.setState({parks: fetchedParks.response.venues});
-              this.state.parks.forEach(item => {
-                item.markerType = 'park';
-              });
-              return this.state.parks;
-            }else{
-              alert("Sorry we couldn't load the data for Parks. Please try again");
-            }
-          }).catch(err => console.log (err));
-      }
+  //fetches the parks in the area
+  fetchParks = () => {
+      fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=2000&query=parcul&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
+      .then(result => result.json())
+      .then(fetchedParks => {
+          //if there are items in the venue, adds them to the parks state
+          if(fetchedParks.response.venues.length >0){
+            this.setState({parks: fetchedParks.response.venues});
+            this.state.parks.forEach(item => {
+              item.markerType = 'park';
+            });
+            return this.state.parks;
+          }else{
+            alert("Sorry we couldn't load the data for Parks. Please try again");
+          }
+        }).catch(err => console.log (err));
+    }
 
-      //fetches the theaters in the area
-      fetchTheaters = () => {
-        fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=1500&query=teatru&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
-        .then(result => result.json())
-        .then(fetchedTheaters => {
-            //if there are items in the venue, adds them to the parks state
-            if(fetchedTheaters.response.venues.length >0){
-              this.setState({theaters: fetchedTheaters.response.venues});
-              this.state.theaters.forEach(item => {
-                item.markerType = 'theater';
-              });
-              return this.state.theaters;
-            }else{
-              alert("Sorry we couldn't load the data for Theaters. Please try again");
-            }
-          }).catch(err => console.log (err));
-      }
+    //fetches the theaters in the area
+    fetchTheaters = () => {
+      fetch('https://api.foursquare.com/v2/venues/search?ll=47.6507275,23.5765156&intent=browse&radius=1500&query=teatru&client_id=3T544WQKFOVHSHX5DJW5VOILONS4NQEX0APKY1XSXBZW2EFF&client_secret=0XTVXJHYBHSW3Q55A1MEN1L3HDU1NDARNC0JJ4RPIJPEHFWD&v=20180708')
+      .then(result => result.json())
+      .then(fetchedTheaters => {
+          //if there are items in the venue, adds them to the parks state
+          if(fetchedTheaters.response.venues.length >0){
+            this.setState({theaters: fetchedTheaters.response.venues});
+            this.state.theaters.forEach(item => {
+              item.markerType = 'theater';
+            });
+            return this.state.theaters;
+          }else{
+            alert("Sorry we couldn't load the data for Theaters. Please try again");
+          }
+        }).catch(err => console.log (err));
+    }
 
     //when all the data are fetched updates the mapMarkers state
     updateState = () => {
@@ -107,6 +107,7 @@ class App extends Component {
       let allMarkers= [...this.state.museums, ...this.state.parks, ...this.state.cinemas, ...this.state.theaters];
       //and adds it to the state
       this.setState({mapMarkers: allMarkers});
+      console.log (this.state.mapMarker);
     }
 
 
@@ -150,12 +151,11 @@ class App extends Component {
       console.log('menu button clicked');
       if (this.state.sideBarStyle.display === 'none') {
         //if the sidebar is closed (hidden) makes it visible according to it's initial style
-        this.setState({sideBarStyle: {display: 'block'}});
+        this.setState({sideBarStyle: {display: 'inherit'}});
       }else{
           //if the sidebar is opened (visible) makes it hiden
         this.setState({sideBarStyle: {display: 'none'}});
       }
-      /*return this.state.sideBarStyle;*/
     }
 
   componentDidMount(){
